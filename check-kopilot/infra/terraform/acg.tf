eefc5e83177813624d96a105f9dd2d927f2946cb
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2026-07-27 21:43:40 +0900

    fix(infra): Fix terraform fmt (#69)

--- check-kopilot/infra/terraform/acg.tf
+++ check-kopilot/infra/terraform/acg.tf
@@ -1,6 +1,6 @@
 resource "ncloud_access_control_group" "db_acg" {
-  name   = "team3-db-acg"
-  vpc_no = data.ncloud_vpc.vpc.id
+  name        = "team3-db-acg"
+  vpc_no      = data.ncloud_vpc.vpc.id
   description = "Database Access Control Group for team3"
 }
 
@@ -20,7 +20,7 @@ resource "ncloud_access_control_group_rule" "db_acg_rule" {
     port_range  = "6379"
     description = "Allow Redis traffic from NKS Node Subnet"
   }
-  
+
   outbound {
     protocol   = "TCP"
     ip_block   = "0.0.0.0/0"
