34c49ca9c04334697217852e1694e4edd2707da9
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2026-07-27 21:50:39 +0900

    fix(infra): Add required arguments for db infra (#70)

--- check-kopilot/infra/terraform/db.tf
+++ check-kopilot/infra/terraform/db.tf
@@ -3,6 +3,7 @@ resource "ncloud_mysql" "mysql" {
   server_name_prefix = "team3-db"
   vpc_no             = data.ncloud_vpc.vpc.id
   subnet_no          = ncloud_subnet.private_db.id
+  host_ip            = "192.168.152.10"
 
   image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.MYSQL.8033.B050"
   product_code       = "SVR.VSVR.STAND.C002.M008.NET.SSD.B050.G002"
@@ -16,11 +17,18 @@ resource "ncloud_mysql" "mysql" {
   database_name = "kopilot"
 }
 
+resource "ncloud_redis_config_group" "redis_config" {
+  name          = "team3-redis-config"
+  redis_version = "7.0.15-simple"
+}
+
 resource "ncloud_redis" "redis" {
   service_name       = "team3-redis"
   server_name_prefix = "team3-rd"
   vpc_no             = data.ncloud_vpc.vpc.id
   subnet_no          = ncloud_subnet.private_db.id
+  config_group_no    = ncloud_redis_config_group.redis_config.id
+  mode               = "SIMPLE"
 
   image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.REDIS.7015.B050"
   product_code       = "SVR.VSVR.STAND.C002.M008.NET.SSD.B050.G002"
